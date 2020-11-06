746cf807a05d326a540c955462791e9326093c02
Karthik Nishanth <Karthik.nishanth06@gmail.com>
2020-11-06 20:24:57 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,101 @@
+import { useEffect } from "react";
+import GameDetail from "../components/GameDetail";
+import { useDispatch, useSelector } from "react-redux";
+import { loadGames } from "../actions/gameAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import { fadeIn } from "../animation";
+function Home() {
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+  const { popular, newGames, upcoming, searched } = useSelector(
+    (state) => state.games
+  );
+  return (
+    <GameList variants={fadeIn} initial="hidden" animate="show">
+      <AnimateSharedLayout type="crossfade">
+        <AnimatePresence>
+          {pathId && <GameDetail pathId={pathId} />}
+        </AnimatePresence>
+        {searched.length ? (
+          <div className="searched">
+            <h2>Searched Games</h2>
+            <Games>
+              {searched.map((game) => (
+                <Game
+                  name={game.name}
+                  released={game.released}
+                  id={game.id}
+                  image={game.background_image}
+                  key={game.id}
+                />
+              ))}
+            </Games>
+          </div>
+        ) : (
+          ""
+        )}
+        <h2>Upcoming Games</h2>
+        <Games>
+          {upcoming.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+        </Games>
+        <h2>Popular Games</h2>
+        <Games>
+          {popular.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+        </Games>
+        <h2>New Games</h2>
+        <Games>
+          {newGames.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+        </Games>
+      </AnimateSharedLayout>
+    </GameList>
+  );
+}
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  margin-bottom: 2rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 2rem;
+  grid-row-gap: 4rem;
+  @media (max-width: 400px) {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
+`;
+export default Home;
